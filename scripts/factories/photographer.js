20a0df3a734c1211e4bbaeeaabdfbfcd2211d66a
Julien Bergé <berge.jul@gmail.com>
2023-05-02 11:38:03 +0200

    etape 2 ok

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,22 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const cityElement = document.createElement('p');
+        cityElement.textContent = city
+        const countryElement = document.createElement('p');
+        countryElement.textContent = country
+        const taglineElement = document.createElement('p');
+        taglineElement.textContent = tagline
+        const priceElement = document.createElement('p');
+        priceElement.textContent = price
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(cityElement);
+        article.appendChild(countryElement);
+        article.appendChild(taglineElement);
+        article.appendChild(priceElement);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
