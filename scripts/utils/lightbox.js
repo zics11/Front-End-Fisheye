4888e0b673e74838cd2349c11dc64cafabfabffb
Julien Bergé <berge.jul@gmail.com>
2023-05-15 18:26:49 +0200

    lighbox ok

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,35 +1,33 @@
+/* eslint-disable no-unused-vars */
 
 // function displayLightboxModal(index) {
 //     const modal = document.getElementById('lightbox-modal');
 //     const modalImage = document.getElementById('modal-image');
-//     const mediaElements = document.querySelectorAll('.media-card');
-//     const imageUrl = mediaElements[index].src;
-//     modalImage.src = imageUrl;
-//     modal.style.display = 'flex';
-// }
 
 function displayLightboxModal(index) {
     const modal = document.getElementById('lightbox-modal');
-    const modalContent = document.getElementById('lightbox-content');
+    const modalContent = document.getElementById('media-content');
     const mediaElements = document.querySelectorAll('.media-card');
+    console.log("mediaElements", mediaElements)
     const mediaElement = mediaElements[index];
-
-    // // Supprimer tous les éléments enfants du contenu modal existant
-    // while (modalContent.firstChild) {
-    //     modalContent.removeChild(modalContent.firstChild);
-    // }
 
     // Créer un nouvel élément de média en fonction du type (image ou vidéo)
     if (mediaElement.classList.contains('image-card')) {
         const imageSrc = mediaElement.src;
+        const imageTitle = mediaElement.alt;
         const imageElement = document.createElement('img');
+        const titleElement = document.createElement('h2')
         imageElement.src = imageSrc;
         imageElement.alt = 'Lightbox Image';
+        imageElement.className = "media"
+        titleElement.textContent = imageTitle;
         modalContent.appendChild(imageElement);
+        modalContent.appendChild(titleElement);
     } else if (mediaElement.classList.contains('video-card')) {
         const videoSrc = mediaElement.src;
         const videoElement = document.createElement('video');
         videoElement.src = videoSrc;
+        videoElement.className = "media"
         videoElement.autoplay = true;
         videoElement.controls = true;
         modalContent.appendChild(videoElement);
@@ -37,4 +35,48 @@ function displayLightboxModal(index) {
 
     // Afficher la modale
     modal.style.display = 'flex';
+}
+
+// Fonction pour afficher le média précédent
+function showPreviousMedia() {
+    const mediaElements = Array.from(document.querySelectorAll('.media-card'));
+    const modalContent = document.getElementById('media-content');
+    const currentImage = modalContent.querySelector('.media').src;
+    const currentIndex = mediaElements.findIndex(element => element.src === currentImage);
+    let previousIndex = currentIndex - 1;
+    if (previousIndex < 0) {
+        previousIndex = currentIndex;
+    }
+    while (modalContent.firstChild) {
+        modalContent.removeChild(modalContent.firstChild);
+    }
+
+    displayLightboxModal(previousIndex);
+}
+
+// Fonction pour afficher le média suivant
+function showNextMedia() {
+    const mediaElements = Array.from(document.querySelectorAll('.media-card'));
+    const modalContent = document.getElementById('media-content');
+    const currentImage = modalContent.querySelector('.media').src;
+    const currentIndex = mediaElements.findIndex(element => element.src === currentImage);
+    let nextIndex = currentIndex + 1;
+    if (nextIndex >= mediaElements.length) {
+        nextIndex = currentIndex;
+    }
+    while (modalContent.firstChild) {
+        modalContent.removeChild(modalContent.firstChild);
+    }
+
+    displayLightboxModal(nextIndex);
+}
+
+
+function closeLightbox() {
+    const modal = document.getElementById("lightbox-modal");
+    const modalContent = document.getElementById('media-content');
+    while (modalContent.firstChild) {
+        modalContent.removeChild(modalContent.firstChild);
+    }
+    modal.style.display = "none";
 }
