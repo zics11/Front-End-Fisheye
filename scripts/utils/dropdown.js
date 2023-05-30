b96984810259af142913b757854d63026fb7c305
Julien <berge.jul@gmail.com>
2023-05-30 11:30:47 +0200

    modif tri part default

--- scripts/utils/dropdown.js
+++ scripts/utils/dropdown.js
@@ -2,6 +2,30 @@ var menu = document.querySelector('.dropdown'); // Bouton du menu déroulant
 var menuButton = document.getElementById('dropdown-btn'); // Bouton du menu déroulant
 var menuList = document.getElementById('dropdown-content'); // Liste des options du menu déroulant
 
+// Écouteur d'événements pour le clic sur le bouton du menu déroulant
+menuButton.addEventListener('click', toggleMenu);
+
+// La fonction toggleMenu est appelée lors du clic sur le bouton du menu déroulant pour afficher ou masquer le menu
+function toggleMenu() {
+  var expanded = menuButton.getAttribute('aria-expanded') === 'true' || false;
+  menuButton.setAttribute('aria-expanded', !expanded);
+  if (expanded) {
+    menu.classList.remove('open');
+  } else {
+    menu.classList.add('open');
+  }
+  menuList.style.display = expanded ? 'none' : 'block';
+}
+
+// La fonction selectOption est appelée lors de la sélection d'une option dans le menu déroulant pour mettre à jour l'option sélectionnée et appeler la fonction selectTri
+function selectOption(option) {
+  menuButton.innerHTML = option;
+  menuButton.setAttribute('aria-expanded', 'false');
+  menuList.style.display = 'none';
+  selectTri(option);
+  menuButton.setAttribute('aria-activedescendant', 'selectedOption');
+}
+
 // La fonction selectTri est appelée pour trier les médias en fonction de l'option sélectionnée dans le menu déroulant
 async function selectTri(option) {
   // eslint-disable-next-line no-undef
@@ -10,7 +34,7 @@ async function selectTri(option) {
 
   if (option === 'Popularité') {
     mediaOrdonnees.sort(function (a, b) {
-      return a.likes - b.likes;
+      return b.likes - a.likes;
     });
     document.querySelector(".photograph-media").innerHTML = "";
     // eslint-disable-next-line no-undef
@@ -36,29 +60,5 @@ async function selectTri(option) {
   }
 }
 
-// La fonction selectOption est appelée lors de la sélection d'une option dans le menu déroulant pour mettre à jour l'option sélectionnée et appeler la fonction selectTri
-function selectOption(option) {
-  menuButton.innerHTML = option;
-  menuButton.setAttribute('aria-expanded', 'false');
-  menuList.style.display = 'none';
-  selectTri(option);
-  menuButton.setAttribute('aria-activedescendant', 'selectedOption');
-}
-
-// La fonction toggleMenu est appelée lors du clic sur le bouton du menu déroulant pour afficher ou masquer le menu
-function toggleMenu() {
-  var expanded = menuButton.getAttribute('aria-expanded') === 'true' || false;
-  menuButton.setAttribute('aria-expanded', !expanded);
-  if (expanded) {
-    menu.classList.remove('open');
-  } else {
-    menu.classList.add('open');
-  }
-  menuList.style.display = expanded ? 'none' : 'block';
-}
-
-// Écouteur d'événements pour le clic sur le bouton du menu déroulant
-menuButton.addEventListener('click', toggleMenu);
-
 // Sélectionne l'option 'Populaire' par défaut lors du chargement de la page
-selectOption('Populaire');
+selectOption('Popularité');
